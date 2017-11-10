51a40b69e6a77edb87befcc0fa8bcd7603b575fb
Tanmay Sinha <tanmaysinha088@gmail.com>
2017-11-11 04:24:38 +0530

    initial commit

--- Finanalytics/settings.py
+++ Finanalytics/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'Finanalytics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fa101fa',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
